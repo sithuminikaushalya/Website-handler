f8f8fab18a69d1349c80e223aee3ab2667c6a2cc
Sithumini Kaushalya <118107125+sithuminikaushalya@users.noreply.github.com>
2024-08-07 13:26:21 +0530

    change the url

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import '../models/gold_rate.dart';
 
 class ApiService {
-  final String baseUrl = 'http://localhost/wordpress/wp-json/daily-updater/v1';
+  final String baseUrl = 'http://10.0.2.2/wordpress/wp-json/daily-updater/v1';
 
   Future<List<GoldRate>> fetchGoldRates() async {
     final response = await http.get(Uri.parse('$baseUrl/details'));
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,3 @@
-const String baseUrl = 'http://localhost/wordpress/wp-json/daily-updater/v1';
+const String baseUrl = 'http://10.0.2.2/wordpress/wp-json/daily-updater/v1';
+
+// when we work with emulator instead of localhost we have to put 10.0.2.2
